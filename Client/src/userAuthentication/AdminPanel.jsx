416cd44c885e99d960a5de417c2a285a8a3c7aee
Sainath Patil <pspatilsai64@gmail.com>
2023-06-19 23:41:35 +0530

    Added view user and logout button in admin panel

--- Client/src/userAuthentication/AdminPanel.jsx
+++ Client/src/userAuthentication/AdminPanel.jsx
@@ -11,31 +11,6 @@ function AdminPanel() {
 
   return (
     <>
-      {/* <div>
-        <Navbar />
-        <h1 className="text-center text-3xl font-bold mt-10">Admin Panel</h1>
-        <div className="flex flex-col items-center justify-center mt-10">
-          <div className="space-x-4 mb-4">
-            <Link to="/viewUser">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                View Users
-              </button>
-            </Link>
-            <Link to="/uploadProduct">
-              <button className="bg-lime-500 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">
-                Upload Products
-              </button>
-            </Link>
-          </div>
-          <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            onClick={logOut}
-          >
-            Logout
-          </button>
-        </div>
-      </div> */}
-
       <div className="flex ">
         <div className="h-screen w-1/5 border bg-gray-800 text-white text-2xl  ">
           <div className="flex flex-col">
@@ -50,13 +25,31 @@ function AdminPanel() {
               </div>
             </div>
             <div className="flex flex-col justify-between p-5 border border-indigo-400 h-[84.8vh]">
-              <div className="border border-rose-300">Buttons</div>
-              <div className="border border-rose-300">logout</div>
+              <div className="">
+                <div className="space-x-4 mb-4">
+                  <Link to="/viewUser">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                      View Users
+                    </button>
+                  </Link>
+                </div>
+              </div>
+              <div className="">
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={logOut}
+                >
+                  Logout
+                </button>
+              </div>
             </div>
           </div>
         </div>
         <div className="h-screen w-4/5 border bg-gray-800 text-white p-5">
-          Content{" "}
+          <h1 className="font-bold text-2xl mb-10">Hey, Welcome Admin</h1>
+          <div className="mb-5 ml-10">
+            <h1 className="font-semibold text-xl ">Admin details</h1>
+          </div>
         </div>
       </div>
     </>
