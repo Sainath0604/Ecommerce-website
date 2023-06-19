6462ac2c169eb9f1da8922840f527de57f38addb
Sainath Patil <pspatilsai64@gmail.com>
2023-06-19 18:38:44 +0530

    Made some changes in admin panel page

--- Client/src/userAuthentication/AdminPanel.jsx
+++ Client/src/userAuthentication/AdminPanel.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
+import logo from "../Logo/logo.png";
+
 function AdminPanel() {
   const logOut = (e) => {
     e.preventDefault();
@@ -9,7 +11,7 @@ function AdminPanel() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <Navbar />
         <h1 className="text-center text-3xl font-bold mt-10">Admin Panel</h1>
         <div className="flex flex-col items-center justify-center mt-10">
@@ -31,6 +33,30 @@ function AdminPanel() {
           >
             Logout
           </button>
+        </div>
+      </div> */}
+
+      <div className="flex ">
+        <div className="h-screen w-1/5 border bg-gray-800 text-white text-2xl  ">
+          <div className="flex flex-col">
+            <div className="flex justify-between items-center p-5 border border-lime-400 h-[15vh]">
+              <div>
+                <Link to="/">
+                  <img className="w-28" src={logo} alt="Your Company" />
+                </Link>
+              </div>
+              <div>
+                <span className="">Admin Panel</span>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between p-5 border border-indigo-400 h-[84.8vh]">
+              <div className="border border-rose-300">Buttons</div>
+              <div className="border border-rose-300">logout</div>
+            </div>
+          </div>
+        </div>
+        <div className="h-screen w-4/5 border bg-gray-800 text-white p-5">
+          Content{" "}
         </div>
       </div>
     </>
